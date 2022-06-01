86e1a2b1b3ed7393b98e696470fd84dcec5fb1cc
kapsner <lorenz.kapsner@web.de>
2022-06-01 21:52:38 +0200

    updated init script for influxdb

--- setup_docker.sh
+++ setup_docker.sh
@@ -4,10 +4,15 @@
 docker build -f Dockerfile -t tempmanager .
 
 # start framework for initial setup
-docker-compose up -d
-
-# wait 30 seconds
-sleep 30
-
-# stop framework
-docker-compose down
+docker run --rm \
+  -e INFLUXDB_DB=$INFLUXDB_DB \
+  -e INFLUXDB_ADMIN_ENABLED=true \
+  -e INFLUXDB_ADMIN_USER=$INFLUXDB_ADMIN_USER \
+  -e INFLUXDB_ADMIN_PASSWORD=$INFLUXDB_ADMIN_PASSWORD \
+  -e INFLUXDB_USER=$INFLUXDB_USER \
+  -e INFLUXDB_USER_PASSWORD=$INFLUXDB_USER_PASSWORD \
+  --env-file ./.env \
+  -v ./influxdb-volume/data:/var/lib/influxdb \
+  -v ./influxdb-volume/config:/etc/influxdb
+  influxdb \
+  /init-influxdb.sh
